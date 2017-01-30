47b1b45e592dbb91ca06d9eaf5250e5699fde754
krishshyam <krishshyam007@gmail.com>
2017-01-30 18:22:26 +0530

    Foreground notifications

---
+++ app/src/main/java/edu/nitt/vortex/ForegroundNotificationService.java
@@ -0,0 +1,27 @@
+package edu.nitt.vortex;
+
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by HP INDIA on 30-01-2017.
+ */
+public class ForegroundNotificationService extends FirebaseMessagingService {
+        @Override
+        public void onMessageReceived(RemoteMessage remoteMessage) {
+            super.onMessageReceived(remoteMessage);
+            Log.d("msg", "onMessageReceived: " + remoteMessage.getData().get("message"));
+            NotificationCompat.Builder builder = new  NotificationCompat.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("Vortex")
+                    .setContentText(remoteMessage.getNotification().getBody());
+            NotificationManager manager = (NotificationManager)     getSystemService(NOTIFICATION_SERVICE);
+            manager.notify(0, builder.build());
+
+
+    }
+}
